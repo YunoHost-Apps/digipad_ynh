eb587a0543feadd4c01ec97a6687c8d395ee0997
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-04-16 22:30:03 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,7 @@
 
 nodejs_version=20
 server_ip=$(curl https://ip.yunohost.org)
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================

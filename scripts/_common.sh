d2878302eb126046dfa608ab03db09e5f4a011d6
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-10-15 23:52:56 +0200

    helpers 2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 nodejs_version=18
 server_ip=$(curl https://ip.yunohost.org)
-
-#=================================================
-# PERSONAL HELPERS
-#=================================================

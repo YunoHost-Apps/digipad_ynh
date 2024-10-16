81296c82a85dc0ba4600b0430ce1f5b9467c1f8b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-10-16 17:40:33 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-key=$(ynh_string_random --length=128)
-public_ip4="$(curl -s https://ip.yunohost.org)"
-
 nodejs_version="20"
